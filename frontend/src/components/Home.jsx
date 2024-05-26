cb67cc3739f26601d337224e7372cc6cd72ad06c
josephpeter231 <josephpeterjcit@gmail.com>
2024-05-26 20:35:23 +0530

    Updated Anchor Tag

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import { Link } from 'react-router-dom'
 const navigation = [
  
 ]
@@ -70,15 +70,7 @@ export default function Example() {
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
+                 
                 </div>
                 <div className="py-6">
                   <a
@@ -111,10 +103,10 @@ export default function Example() {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Discover Your New Home Today!{' '}
-              <a href="/login" className="font-semibold text-indigo-600">
+              <Link to="/login" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="text-center">
@@ -125,12 +117,12 @@ export default function Example() {
                Comfort and Convenience Await!
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/login"
+              <Link
+                to="/login"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
-              </a>
+              </Link>
               <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
                 Discover Homes <span aria-hidden="true">→</span>
               </a>
